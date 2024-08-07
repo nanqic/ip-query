7ae38a1001bc6881342feac0ac472dbcd97d393c
nanqic <nanqic@163.com>
2024-08-07 10:47:45 +0800

    opt format

--- api/getSource.js
+++ api/getSource.js
@@ -1,8 +1,8 @@
 module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    let { code, auth, date } = req.query
-    let url = `https://stream.ziguijia.com/st?code=${code}&format=mp4&date=${date}&auth=${auth}`
+    let { code, auth, date, format, width } = req.query
+    let url = `https://stream.ziguijia.com/st?code=${code}&format=${format || 'mp4'}&width=${width || 480}&date=${date}&auth=${auth}`
     const resp = await fetch(url)
     // console.log(resp.url);
     let targetUrl = resp.url.replace('ziguijia.cn', 'ningway.com');
