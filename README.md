140d3f77d6719d84afe1943064c51c2f493bd9be
nanqic <nanqic@163.com>
2024-11-20 14:06:59 +0800

    opt st

---
+++ README.md
@@ -0,0 +1,3 @@
+# 一些测试接口
+
+- 查询stream url `/st?code=xxx`
--- api/st.js
+++ api/st.js
@@ -3,17 +3,18 @@ module.exports = async function handler(req, res) {
 
     let { code, format, width } = req.query
 
-    let urlBase = `https://ziguijia.com/chatroom/shoot/${code}`
+    // let urlBase = `http://zgj.ningway.com:81/j?code=${code}`
+    let urlBase = `https://ziguijia.com/j?code=${code}`
     const respOne = await fetch(urlBase)
     let text = await respOne.text()
     const regex = /(date=\d+&amp;auth=\w+)/;
     const match = text.match(regex);
-    // console.log(text);
+    // console.log({match});
 
     if (match) {
         let date_auth = match[1]?.replace('amp;', '');
         // let date_auth = 'date=1732073879434&auth=a43fc54bdac09eb'
-        let url = `https://stream.ziguijia.com/st?code=${code}&format=${format || 'mp4'}&width=${width || 480}&${date_auth}`
+        let url = `https://stream.ziguijia.com/st?code=${code}&format=${format || 'mp4'}&width=${width || 720}&${date_auth}`
         const resp = await fetch(url)
         // console.log(resp.url);
         // let targetUrl = resp.url.replace('ziguijia.cn', 'ningway.com');
