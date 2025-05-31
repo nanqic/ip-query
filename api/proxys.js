1e3caa554377649b236238ec2cfaad1dc78e7b8f
Amigo <nanqic@163.com>
2025-05-31 18:34:13 +0800

    u

--- api/proxys.js
+++ api/proxys.js
@@ -1,37 +1,46 @@
 module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    const { q, page } = req.query;
-    const url = `https://ziguijia.com/search?keywords=${q}${page ? '&page=' + page : ''}`;
+    let { q, page } = req.query
+    let url = `https://ziguijia.com/search?keywords=${q}${page?'&page='+page:''}`
 
-    const resp = await fetch(url);
-    let html = await resp.text();
+    const resp = await fetch(url)
+    let text = await resp.text()
+    // console.log(url, page, text.slice(-20));
+    const regx = new RegExp(`<(script|style|footer|button)(.|\n)*?>(.|\n)*?</(script|style|footer|button)>|<!DOC(.|\n)*?<(hr/?)>`)
+    const regx2 = new RegExp(`href="/j\\?code(=\\w{5}(?:&amp;start=\\d{1,5})?)".target="\\w{5,6}"`, "ig")
+    const regx3 = new RegExp(`href="/search\\?auth=\\d{6}&keywords=\\S{1,512}&page=(\\d{1,2})"`, "ig")
 
-    // 清理不需要的标签和文档声明
-    html = html.replace(/<(script|style|footer|button|hr|!DOC)[^>]*>.*?<\/\1\s*>|<\/(script|style|footer|button)>/gis, '');
+    text = text.replace(regx, '')
+    const regxs = new RegExp(`<script(.|\n)*></script>`)
+    text = text.replace(regxs, '')
+    text = text.replace(regx2, (a, b) => {
+        // console.log(a,b);
+        return `onclick=window.open("/video?no${b.replace('amp;', '')}")`
+    })
+    text = text.replace(regx3, (a, b) => {
+        // console.log(a, b, `onclick=window.open("/vsearch/+${b}")`);
+        return `onclick=window.open("/vsearch/${keywords}/?page=${b}")`
+    })
+    text = text
+        .replace(/href="\/search/ig, ' target="_blank" href="/vsearch')
+        .replace(/keywords=/ig, '/').replace(/\?auth=\d+&amp;/ig, '')
+        .replace(/&(?:amp;)page/ig, '?page')
 
-    // 替换视频链接（原regx2）
-    html = html.replace(/href="\/j\?code(=(\w{5})(?:&amp;start=\d{1,5})?)"[^>]*?>/ig, 
-        (_, code) => `onclick="window.open('/video?no${code.replace('amp;', '')}')"`);
-
-    // 替换分页链接（原regx3）
-    html = html.replace(/href="\/search\?auth=\d+&amp;keywords=([^&"]+?)&amp;page=(\d{1,2})"/ig, 
-        (_, keywords, pageNum) => `onclick="window.open('/vsearch/${q}/?page=${pageNum}')"`);
-
-    // 替换其他搜索链接
-    html = html
-        .replace(/href="\/search/ig, 'target="_blank" href="/vsearch')
-        .replace(/(\?|&amp;)auth=\d+&amp;/ig, '$1')
-        .replace(/keywords=([^&"]+?)(&amp;page=|\")/ig, (_, kw, suffix) => 
-            suffix.startsWith('&amp;') ? `?page=${suffix.split('=')[1]}` : '');
-
-    // 添加播放全部功能
-    if (html.includes('/vsearch/player/')) {
-        const codes = [...html.matchAll(/data-code="(\w{5})"/g)]
-            .map(match => match[1])
-            .join(',');
-        html = html.replace('/vsearch/player/', `/search?codes=${codes}&keywords=`);
-    }
-
+    // 播放全部 链接替换
+    text = text.replace(/\/vsearch\/player\??\//, `/search?codes=${getCodes(text)}&keywords=`)
     res.setHeader('Content-Type', 'text/html');
-    return res.send(html);
-};
+
+    return res.send(text);
+}
+
+
+function getCodes(text) {
+    const regx = new RegExp(`data-code="(\\w{5})"`, "ig")
+    let codes = ''
+    let match;
+    while ((match = regx.exec(text)) !== null) {
+        const codeValue = match[1];
+        codes += `${codeValue},`
+    }
+    return codes.slice(0, -1)
+}
